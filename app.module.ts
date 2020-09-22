e89fd7cf14a6148103f0f1ea6ffd86bba6e28ed1
mniians <myers.nii-ansah@caseware.com>
2020-09-22 12:49:22 -0400

    initial project structure

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecipeBookComponent,
+    ShoppingListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
