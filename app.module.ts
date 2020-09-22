a46e2f519568c01cfead8e40c0f4c3817947c330
mniians <myers.nii-ansah@caseware.com>
2020-09-22 13:22:51 -0400

    created recipe item component

--- app.module.ts
+++ app.module.ts
@@ -7,6 +7,7 @@ import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.compo
     RecipeBookComponent,
     ShoppingListComponent,
     HeaderComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
