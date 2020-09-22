90d67630d307f658cf4fbda210409168502778c0
mniians <myers.nii-ansah@caseware.com>
2020-09-22 13:02:34 -0400

    added header component

--- app.module.ts
+++ app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeBookComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
